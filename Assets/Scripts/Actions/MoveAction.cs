c270de60f6b7fd31421ad29965b4c57196d0d7cc
Jormii <lopeznatalj@gmail.com>
2020-06-13 21:43:14 +0200

    Actions formatted

--- Assets/Scripts/Actions/MoveAction.cs
+++ Assets/Scripts/Actions/MoveAction.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MoveAction : IAction
+﻿public class MoveAction : IAction
 {
     public enum Destination
     {
--- Assets/Scripts/Actions/MoveToStoreAction.cs
+++ Assets/Scripts/Actions/MoveToStoreAction.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MoveToStoreAction : MoveAction
+﻿public class MoveToStoreAction : MoveAction
 {
     public readonly int STORE_ID;
 
